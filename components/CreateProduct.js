02de8a3af38e78c8fa66b7548ae0a6fad5e554d0
Mateusz Jarzębowski-Bownik <mateusz.jarzebowski.bownik@gmail.com>
2021-09-08 17:15:07 +0200

    Added product uploads

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,20 +1,66 @@
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import { useMutation } from '@apollo/client';
+import gql from "graphql-tag";
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products'
+import Router from 'next/router';
+
+const CREATE_PRODUCT_MUTATION = gql`
+    mutation CREATE_PRODUCT_MUTATION(
+        $name: String!
+        $description: String!
+        $priceBuy: Int!
+        $priceRent: Int!
+        $image: Upload
+    ) {
+        createProduct(
+            data: {
+                name: $name
+                description: $description
+                priceBuy: $priceBuy
+                priceRent: $priceRent
+                status: "AVAILABLE"
+                photo: { create: { image: $image, altText: $name } }
+            }
+        ) {
+            id
+            priceBuy
+            priceRent
+            description
+            name
+        }
+    }
+`;
 
 export default function CreateProduct() {
-    const { inputs, handleChange } = useForm({
+    const { inputs, handleChange, clearForm, resetForm } = useForm({
         image: '',
         name: 'heyyy',
-        price: 123,
+        priceBuy: 12300,
+        priceRent: 123,
         description: 'yoooooo, description'
     });
 
+    const [createProduct, { loading, error, data }] = useMutation(
+        CREATE_PRODUCT_MUTATION,
+        {
+          variables: inputs,
+          refetchQueries: [{ ALL_PRODUCTS_QUERY }]
+        }
+      );
+
     return (
-        <Form onSubmit={(e) => {
+        <Form onSubmit={async (e) => {
             e.preventDefault();
-            console.log(inputs);
+            const res = await createProduct();
+            clearForm();
+            Router.push({
+                pathname: `product/${res.data.createProduct.id}`
+            })
         }}>
-            <fieldset>
+            <DisplayError error={error}/>
+            <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="name">
                     Image
                     <input
@@ -37,15 +83,27 @@ export default function CreateProduct() {
                         onChange={handleChange}
                     />  
                 </label>
-                <label htmlFor="price">
-                    Price
+                <label htmlFor="priceBuy">
+                    Price - Buy
                     <input
                         required
                         type="number"
-                        id="price"
-                        name="price"
-                        placeholder="price"
-                        value={inputs.price}
+                        id="priceBuy"
+                        name="priceBuy"
+                        placeholder="priceBuy"
+                        value={inputs.priceBuy}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor="priceRent">
+                    Price - Rent
+                    <input
+                        required
+                        type="number"
+                        id="priceRent"
+                        name="priceRent"
+                        placeholder="priceRent"
+                        value={inputs.priceRent}
                         onChange={handleChange}
                     />
                 </label>
