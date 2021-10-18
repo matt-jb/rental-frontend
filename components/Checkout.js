12256518bc6547d1d581fe01aa8898b9c5987ebc
Mateusz Jarzębowski-Bownik <mateusz.jarzebowski.bownik@gmail.com>
2021-10-18 17:01:51 +0200

    Coding checkout

--- components/Checkout.js
+++ components/Checkout.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { useState } from 'react';
+import nProgress from 'nprogress';
 import CheckoutButton from './styles/CheckoutButton';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -12,20 +21,78 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`
+
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
-export default function Checkout() {
-  function handleSubmit(e) {
+function CheckoutForm() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [ checkout, { error: graphQLError } ] = useMutation(CREATE_ORDER_MUTATION)
+  async function handleSubmit(e) {
+    // 1. Stop the form from submitting and turn the loader one
     e.preventDefault();
+    setLoading(true);
     console.log('We gotta do some work..');
+    // 2. Start the page transition
+    nProgress.start();
+    // 3. Create the payment method via stripe (Token comes back here if successful)
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+    console.log(paymentMethod);
+    // 4. Handle any errors from stripe
+    if (error) {
+      setError(error);
+      nProgress.done();
+      return;
+    }
+    // 5. Send the token from step 3 to our keystone server, via a custom mutation!
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id
+      }
+    });
+    console.log(`Finished with the order!`);
+    console.log(order);
+    // 6. Change the page to view the order
+    // 7. Close the cart
+
+    // 8. turn the loader off
+    setLoading(false);
+    nProgress.done();
   }
 
   return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      {graphQLError && <p style={{ fontSize: 12 }}>{graphQLError.message}</p>}
+      <CardElement />
+      <CheckoutButton>Check Out Now</CheckoutButton>
+    </CheckoutFormStyles>
+  );
+}
+
+export default function Checkout() {
+  return (
     <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <CheckoutButton>Buy now!</CheckoutButton>
-      </CheckoutFormStyles>
+      <CheckoutForm />
     </Elements>
   );
 }
+
